d839946fba41ffdf769012ad218f17d88bca90ca
raghav <raghavsarin12@gmail.com>
2024-01-02 23:20:53 +0800

    added readme, created flutter project, added basic app functions

--- sdgproject/lib/main.dart
+++ sdgproject/lib/main.dart
@@ -11,6 +11,6 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Onboarding());
+    return const MaterialApp(home: Onboarding());
   }
 }
